538b8162a44cc00cdc6a35aa0104c0718e432c98
Vedant <vedantrahane01@gmail.com>
2020-03-25 01:11:44 -0400

    Length of longest substring using sliding window method

---
+++ src/com/leetcode/practice/SlidingWindowLongestSubstring.java
@@ -0,0 +1,33 @@
+package com.leetcode.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SlidingWindowLongestSubstring {
+
+  public static int lengthOfLongestSubstring(String s) {
+
+    int n = s.length(), ans = 0;
+
+    Map<Character, Integer> map = new HashMap<>();
+
+    for (int j = 0, i = 0; j < n; j++) {
+      if (map.containsKey(s.charAt(j))) {
+        i = Math.max(map.get(s.charAt(j)), i);
+      }
+
+      ans = Math.max(ans, j - i + 1);
+      map.put(s.charAt(j), j + 1);
+    }
+
+
+    return ans;
+  }
+
+
+  public static void main(String[] args) {
+    int len = SlidingWindowLongestSubstring.lengthOfLongestSubstring("pwwkew");
+    System.out.println("ans = " + len);
+  }
+
+}
